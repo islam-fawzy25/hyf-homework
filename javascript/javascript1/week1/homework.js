4a3f94b40d6366525c1363ce61329ae65d16cdfe
Your Name <you@example.com>
2021-01-20 15:41:34 +0100

    done with first task  A future age calculator

---
+++ javascript/javascript1/week1/homework.js
@@ -0,0 +1,11 @@
+  // Home work
+
+
+// Age-ify (A future age calculator) 
+
+const yearOfBirth = 1987
+const yearFuture = 2027
+const age = yearFuture-yearOfBirth
+
+console.log('You will be',(age),'years old in 2027')
+
