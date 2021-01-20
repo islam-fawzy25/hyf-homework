13654c9405b61361aa30b0381491ec5980dd6f56
Your Name <you@example.com>
2021-01-20 15:45:58 +0100

    implemented task2 A dog age calculator

--- javascript/javascript1/week1/homework.js
+++ javascript/javascript1/week1/homework.js
@@ -9,3 +9,17 @@ const age = yearFuture-yearOfBirth
 
 console.log('You will be',(age),'years old in 2027')
 
+// Goodboy-Oldboy (A dog age calculator)
+
+const dogYearOfBirth = 2017;
+const dogYearFuture = 2027;
+
+const shouldShowResultInDogYears  = true;
+
+let dogYear;
+
+
+if(shouldShowResultInDogYears){dogYear = (dogYearFuture - dogYearOfBirth)* 7;}
+else{dogYear = dogYearFuture - dogYearOfBirth;}
+console.log(dogYear);
+
