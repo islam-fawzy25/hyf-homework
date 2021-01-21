d63ea9b3b8ecea75f10ebfbbde27a6119ee4fb3a
Your Name <you@example.com>
2021-01-21 20:31:15 +0100

    impelemnted feedback

--- javascript/javascript1/week1/homework.js
+++ javascript/javascript1/week1/homework.js
@@ -11,65 +11,72 @@ console.log('You will be',(age),'years old in 2027')
 
 // Goodboy-Oldboy (A dog age calculator)
 
-const dogYearOfBirth = 2017;
-const dogYearFuture = 2027;
+const dogYearOfBirth = 2017
+const dogYearFuture = 2027
 
-const shouldShowResultInDogYears  = true;
+const shouldShowResultInDogYears  = true
 
-let dogYear;
+let dogYear
 
 
-if(shouldShowResultInDogYears){dogYear = (dogYearFuture - dogYearOfBirth)* 7;}
-else{dogYear = dogYearFuture - dogYearOfBirth;}
-console.log(dogYear);
+if(shouldShowResultInDogYears){
+  dogYear = (dogYearFuture - dogYearOfBirth)* 7
+}else{
+  dogYear = dogYearFuture - dogYearOfBirth
+}
+console.log(dogYear)
 
 //Housey pricey (A house price estimator)
 
 //housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 // Advise to peter
 
-let wide = 8;
-let deep = 10 ;
-let high = 10;
-let volumeInMeters  =wide * deep * high;
+let wide = 8
+let deep = 10 
+let high = 10
+let volumeInMeters  =wide * deep * high
 let gardenSizeInM2 = 100
 let houseCost = 20500000
-let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300 ;
+let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300 
 
 const salesAdviseText1 = 'paying too much'
 const salesAdviseText2 = 'paying too little'
-let salesAdvise ;
+let salesAdvise 
 let clientName = 'peter '
 
- if (houseCost>housePrice ) { salesAdvise = clientName +salesAdviseText1  
- }else{salesAdvise = clientName + salesAdviseText2 }
+ if (houseCost>housePrice ) {
+    salesAdvise = clientName +salesAdviseText1  
+ }else{
+   salesAdvise = clientName + salesAdviseText2
+   }
 
  console.log(salesAdvise)
 
 // Advise to julia 
- wide = 5;
- deep = 11 ;
- high = 8;
-  volumeInMeters  =wide * deep * high;
- gardenSizeInM2 = 70;
- houseCost = 1000000 ;
-housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300 ;
+ wide = 5
+ deep = 11 
+ high = 8
+  volumeInMeters  =wide * deep * high
+ gardenSizeInM2 = 70
+ houseCost = 1000000 
+housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300 
 clientName ='julia '
-if (houseCost>housePrice ) { salesAdvise = clientName + salesAdviseText1
-    
-}else{salesAdvise = clientName + salesAdviseText2 }
-
+if (houseCost>housePrice ) { 
+  salesAdvise = clientName + salesAdviseText1  
+}else{
+  salesAdvise = clientName + salesAdviseText2 
+}
 console.log(salesAdvise)
 
 
 // 4. Ez Namey (Startup name generator)
 
 const firstWords =["Easy ","Awesome ","Amazing ","Majestic ","Wonderful ","Surprising ",
-"Incredible ","Hit ","Success ","victory " ];
+"Incredible ","Hit ","Success ","victory " ]
 const secondWords =["Corporation","Company","Partners","Agency", "Business",
-"Agent","Team","Train","Crew","Squad"];
-const randomNumber = Math.floor(Math.random() * 10) + 0;
+"Agent","Team","Train","Crew","Squad"]
+const randomNumber = Math.floor(Math.random() * 10) + 0
 
-let startupName =firstWords[randomNumber] + secondWords[randomNumber];
+let startupName =firstWords[randomNumber] + secondWords[randomNumber]
 
 console.log("The startup:" + startupName +" "+ startupName.length +" characters")
