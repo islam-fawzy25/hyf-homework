c0bfc587d540dc1427439d69356e9c93d4ac34fd
Your Name <you@example.com>
2021-01-26 15:06:36 +0100

    implement home work wee2 exercise 2

--- javascript/javascript1/week2/Event-application.js
+++ javascript/javascript1/week2/Event-application.js
@@ -1,17 +1,23 @@
 
 //Home work week2 //Exercise2 //Event application
 
-//i tried to make it simple 
+/* i found this get day date  info in at this page
+ "https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript"
+ i tried to make it simple
 
-const weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',  'Sunday']
-//const today = weekdays[6]
+*/
+
+const weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',  'Sunday']  
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
 
 function getEventWeekday(newDate){
-  
-    return weekdays [(newDate-1)%7] ;
+   return weekdays [(dd+ newDate)%7] ;
 }
 
-console.log(getEventWeekday(5));
-console.log(getEventWeekday(15));
+console.log(getEventWeekday(1));
+console.log(getEventWeekday(8));
+
 
 
