48cf7346cb23b85e705ecfb5d6562720172ab697
Your Name <you@example.com>
2021-01-26 14:11:39 +0100

    implement home work exercise 2

--- javascript/javascript1/week2/Event-application.js
+++ javascript/javascript1/week2/Event-application.js
@@ -1,1 +1,17 @@
-console.log(hello)
+
+//Home work week2 //Exercise2 //Event application
+
+//i tried to make it simple 
+
+const weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',  'Sunday']
+//const today = weekdays[6]
+
+function getEventWeekday(newDate){
+  
+    return weekdays [(newDate-1)%7] ;
+}
+
+console.log(getEventWeekday(5));
+console.log(getEventWeekday(15));
+
+
