a984f537cd58e480f17bb383b598fb266a25aff9
Your Name <you@example.com>
2021-01-29 12:12:43 +0100

    solved for loop problem

--- javascript/javascript1/week2/candyFunction.js
+++ javascript/javascript1/week2/candyFunction.js
@@ -1,11 +1,10 @@
-console.log('yes')
+// Candy Calculater
+
 
 
 const boughtCandyPrices =[];
 const pricePerGram = [0.5,0.7,1.1,0.03];
 var amountToSpend = Math.random() *100 ;
-
-
 
 function addCandy(candyType,weight){
 
@@ -21,61 +20,41 @@ function addCandy(candyType,weight){
             }else if(candyType =='Chewing-gum'){
                 const candyPrice = weight * pricePerGram[3];
                   boughtCandyPrices.push(candyPrice);
-                }/*else if(canBuyMoreCandy()== true){
-                   return console.log('You can buy more, so please do!')
-
-                }else{
-                  return  console.log('Enough candy for you!')
-
-                }*/
-
-}
-
-//boughtCandyPrices.forEach(canBuyMoreCandy);
+                }
+            }
 
 function canBuyMoreCandy (){
-    let totalPrice ='';
 
-//totalPrice += items
-
+let totalPrice = 0 ;
 
     for ( let i = 0 ; i < boughtCandyPrices.length ;i++ ){
-        totalPrice += boughtCandyPrices[i] 
-        
-    }   // i know (for ) not working  also  with foreach so 
-    // i will tried again with while and push again or if you can tell me please why its not working
-   // return console.log(totalPrice)
+        totalPrice  +=  boughtCandyPrices[i] ;
+    } 
 
-    
-    if(amountToSpend >totalPrice)
+    if( amountToSpend < totalPrice  )
         {
         true;
-        console.log('You can buy more, so please do!')
-
-       return 
+        console.log('You can buy more, so please do!');
+       return ;
     }else{
-        console.log('Enough candy for you!')
-
+        console.log('Enough candy for you!');
         false ;
-
-        return 
-    } 
-    
-    
+        return ;
+    }    
 }
 
 
 //console.log(amountToSpend)
-
 addCandy('Sweet',20);
 addCandy('Toffee',10);
-addCandy('Chocolate',10);
+addCandy('Chocolate',15);
 addCandy('Chewing-gum',500);
 //addCandy('Chewing-gum',50000);
-
 
 console.log(boughtCandyPrices);
 canBuyMoreCandy();
 
-//console.log(totalPrice)
-//console.log(boughtCandyPrices[i])
+
+
+
+
