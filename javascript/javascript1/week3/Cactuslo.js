bd35e774c98042f1e8bbb0b8af97573418632b45
Your Name <you@example.com>
2021-02-04 19:21:38 +0100

    implement unique feature with Hanieh at Note app exercise

--- javascript/javascript1/week3/Cactuslo.js
+++ javascript/javascript1/week3/Cactuslo.js
@@ -1,14 +1,13 @@
 //Adding an activity
 let activities = [];
 let  totalDurationActivities=0;
-var today  = new Date();
+var today  = new Date().toLocaleDateString("en-UE");
 
 function addActivity(date , activity , duration){
 activities.push({date , activity , duration});
-date = today.toLocaleDateString("en-US");
-
-
+//date = today.toLocaleDateString("en-UE");
 };
+
 addActivity(today, "Youtube", 30);
 addActivity("24/7-18", "Facebook", 30);
 addActivity("25/7-18", "Instgram", 18);
@@ -19,18 +18,19 @@ console.log(activities)
 //Show my status
 
 function showStatus(){
-for(i in activities){
+ for(i in activities){
    totalDurationActivities += activities[i].duration ;
-   status = 'You have added '+activities.length +' activities.They amount to '+totalDurationActivities + ' min.og usage.';
+   statusForLoop = 'You have added '+activities.length +' activities.They amount to '+totalDurationActivities + ' min.of usage.';
 
    //Usage limit
-
-}if(totalDurationActivities>=100){
-    status = 'You have reached your limit,no more smartphoning for you!'
+   if(totalDurationActivities>=100){
+    statusIf = 'You have reached your limit,no more smartphoning for you!'
+   }
+ }
+    console.log(statusForLoop);
+    console.log(statusIf);
+    return
 }
-console.log(status);
-return
 
-}
 showStatus(activities);
 
--- javascript/javascript1/week3/Step3.js
+++ javascript/javascript1/week3/Step3.js
@@ -42,3 +42,68 @@ const firstNote = getNote(1);
  // The note with id: 1, has the following note text: Pick up groceries
  // The note with id: 2, has the following note text: Do laundry
 
+/*
+ down here it is idea if guest forget number of note and 
+ want to finde note with searchingby one word from this note 
+  WE worked together with Hanieh 
+ */
+ function getWord(word) {
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].content.indexOf(word) != - 1) {
+            return `You are looking for id:${notes[i].id} ${notes[i].content}`;
+        };
+    }
+};
+let Note = getWord('up');
+console.log(Note); 
+
+
+function getWord(word) {
+    let isIncluded = false;
+    for (let i = 0; i < notes.length; i++) {
+        let str = notes[i].content.split(" ");
+        if (str.includes(word)) {
+            console.log(`You are looking for id:${notes[i].id} ${notes[i].content}`);
+            isIncluded = true;
+        }
+    }
+    if (isIncluded === false) {
+        console.log(`You do not have any task including ${word}`);
+    }
+}
+getWord("laundry");
+getWord("up");
+
+
+
+
+
+// down here third project to solve the idea 
+
+ //var str = [{content:"This is an interesting sentence.",id:20},{}];
+ //var response = notes[i].content.split(" ");
+
+ //console.log(response)
+ /* Expected result:
+   This,is,an,interesting,sentence.
+   (Note: returns array)
+   */
+/*
+  function getNote(content){
+    // your code here
+
+   for(let i=0; i< notes.length; i++){ 
+    var response = notes[i].content.split(" ");
+console.log(response)
+     if ( notes[i].content == first){
+
+      return   notes[i]
+     }
+    }
+    return 'Error'
+  };
+  const first = getNote('lundry');
+
+   console.log(first); // {content: 'Pick up groceries', id: 1}
+   */
+  
