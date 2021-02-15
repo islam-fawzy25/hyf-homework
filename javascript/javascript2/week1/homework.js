d1635ab9299caf48196651402de0d05f45c7b872
Your Name <you@example.com>
2021-02-15 11:52:48 +0100

    ya

--- javascript/javascript2/week1/homework.js
+++ javascript/javascript2/week1/homework.js
@@ -1,13 +1,18 @@
 // Problem solving cardio
 // Find the shortest word
 
-console.clear()
+
 const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
+console.log(danishWords.length)
+
+
+
 function totalLetters(danishWords){
 
-   for(i=0;i<danishWords.length ;i++){
+   for(i = 0; i< 7 ;i++){
 const splitWords = danishWords[i].split('')
 console.log(splitWords)
+
    }
 
 }; // returns 'ø'
