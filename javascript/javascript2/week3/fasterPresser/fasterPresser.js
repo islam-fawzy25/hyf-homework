090937ed5c6c4cf6ebf58e1e7c04bf4a73f11df2
Your Name <you@example.com>
2021-03-04 14:14:17 +0100

    Extra features implemented

--- javascript/javascript2/week3/fasterPresser/fasterPresser.js
+++ javascript/javascript2/week3/fasterPresser/fasterPresser.js
@@ -1,6 +1,5 @@
 //The fastest presser in this realm
 
-
 const input = document.getElementById('input');
 const button = document.getElementById('btn');
 const printL = document.getElementById('printL');
@@ -9,7 +8,13 @@ const pressKey = document.querySelector('body');
 let counterS = 0;
 let counterL = 0;
 const winnerMsg = document.getElementById('winner');
+const restart = document.getElementById('restart');
 
+//seconds counter
+const timer = document.getElementById('timer');
+const time = input.value;
+
+// Function to show only press count of  specifice keys 'S' &'L' 
 const printer = function (e) {
     if (e.key == 'l') {
         counterL++
@@ -19,26 +24,57 @@ const printer = function (e) {
         printS.innerText = counterS
     };
 };
-
+// function for getting value of input
 const durationTime = function () {
     return input.value
 };
 
-button.addEventListener('click', function () {
+// The main function that works when we click start game 
+const game = function () {
     pressKey.addEventListener('keydown', printer);
-    const time = setTimeout(() => {
+    setTimeout(() => {
+        //Here we remove, so player press can't count after time
         pressKey.removeEventListener('keydown', printer);
         if (counterL > counterS) {
             winnerMsg.innerText = 'Player L winner '
-        } else if(counterS >counterL){ 
-            winnerMsg.innerText = 'Player S winner ' 
-        }else if(counterL ==0 && counterS ==0 ){
-            winnerMsg.innerText='Hey... Lets play'
-        }else{
-            winnerMsg.innerText='WOoOw .. Drawing ..Try Again!'
+        } else if (counterS > counterL) {
+            winnerMsg.innerText = 'Player S winner '
+        } else if (counterL == 0 && counterS == 0) {
+            winnerMsg.innerText = 'Hey... Lets play'
+        } else {
+            winnerMsg.innerText = ' Draw Game ..Try Again!'
         };
     }, 1000 * durationTime());
-});
+    let timeCounter = durationTime();
+
+    const timerPrint = function () {
+        console.log('hi');
+        if (timeCounter >= 0) {
+            timer.innerHTML = timeCounter
+            timeCounter--;
+        }
+    }
+    //im trying to stop counter after game end, but dosent success
+    let interval = setInterval(timerPrint, 1000);
+
+    if (timeCounter >= 0) {
+        return interval
+    } else { clearInterval(interval) }
+
+}
+button.addEventListener('click', game);
+
+// restart game function 
+const restartGame = function () {
+    counterL = 0;
+    counterS = 0;
+    printL.innerText = counterL;
+    printS.innerText = counterS;
+    game();
+};
+restart.addEventListener('click', restartGame);
+
+
 
 
 
