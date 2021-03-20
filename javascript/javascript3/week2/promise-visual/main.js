c49417ada8ea6ae5e08c947bde90ec77a4828685
Your Name <you@example.com>
2021-03-20 13:44:41 +0100

    implement move elements with promise all

--- javascript/javascript3/week2/promise-visual/main.js
+++ javascript/javascript3/week2/promise-visual/main.js
@@ -12,35 +12,46 @@ const blueBoxT = blueBox.offsetTop
 const greenBoxL = greenBox.offsetLeft
 const greenBoxT = greenBox.offsetTop
 
-// One by one 
+// Circels target 
+const redTarget = { x: 20 - redBoxL, y: 300 - redBoxT }
+const blueTarget = { x: 400 - blueBoxL, y: 300 - blueBoxT }
+const greenTarget = { x: 400 - greenBoxL, y: 20 - greenBoxT }
 
+// One by one 
 async function translateOneByOne() {
     try {
-        await moveElement(document.querySelector('ul.marks li:nth-child(1)')
-            , { x: 20 - redBoxL, y: 300 - redBoxT }).then(() => {
-                console.log("Element red has been moved");
-            })
-        await moveElement(document.querySelector('ul.marks li:nth-child(2)')
-            , { x: 400 - blueBoxL, y: 300 - blueBoxT }).then(() => {
-                console.log("Element blue has been moved");
-            })
-        await moveElement(document.querySelector('ul.marks li:nth-child(3)')
-            , { x: 400 - greenBoxL, y: 20 - greenBoxT }).then(() => {
-                console.log("Element green has been moved");
-            })
+        await moveElement(redBox, redTarget)
+            .then(() => { console.log("Element red has been moved"); })
+        await moveElement(blueBox, blueTarget)
+            .then(() => { console.log("Element blue has been moved"); })
+        await moveElement(greenBox, greenTarget)
+            .then(() => { console.log("Element green has been moved"); })
     } catch { console.log('Error!!'); }
 }
-// translateOneByOne()
+//translateOneByOne()
 
-//All at once 
+//All at once with async
 async function translateAllAtOnce() {
-    moveElement(document.querySelector('ul.marks li:nth-child(1)')
-        , { x: 20 - redBoxL, y: 300 - redBoxT })
-    moveElement(document.querySelector('ul.marks li:nth-child(2)')
-        , { x: 400 - blueBoxL, y: 300 - blueBoxT })
-
-    moveElement(document.querySelector('ul.marks li:nth-child(3)')
-        , { x: 400 - greenBoxL, y: 20 - greenBoxT })
-console.log('All Elements have been moved ');
+   try{
+    moveElement(redBox, redTarget)
+    moveElement(blueBox, blueTarget)
+    moveElement(greenBox, greenTarget)
+    console.log('All Elements have been moved ');
+   }catch{console.log('Error!!');}
+    
 }
-translateAllAtOnce()
+//translateAllAtOnce()
+
+
+// All at once promise.all
+// const promisAllAtOnce = Promise.all([
+//     moveElement(redBox, redTarget),
+//     moveElement(blueBox, blueTarget),
+//     moveElement(greenBox, greenTarget)
+// ])
+    // .then(() => console.log('All Elements have been moved'))
+    // .catch(() => console.log('Error!!'))
+
+    // moveElement functions inside promise.all and i did not call promise.all and
+    // it works  already , how can i controll this ,i mean 
+    // no circle moves before i call promise without using function  ?!
