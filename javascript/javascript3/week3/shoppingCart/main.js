d7dfddbf05d50bd015a72f88a878525f3ef2110a
Your Name <you@example.com>
2021-03-23 13:14:39 +0100

    work on shopping cart

--- javascript/javascript3/week3/shoppingCart/main.js
+++ javascript/javascript3/week3/shoppingCart/main.js
@@ -1,84 +1,109 @@
+//Shopping cart using Classes
+//Get some elements to invoke function on it 
+const input = document.getElementById('searchBar')
+const main = document.querySelector('main')
+const button = document.getElementById('button')
+const section = document.querySelector('section')
+const header = document.querySelector('header')
+
 class Product {
-    constructor(name, price) {
-      this.name = name;
-      this.price = price;
-    }
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
   }
-  
-  class ShoppingCart {
-    constructor() {
-      this.products = [];
-     
+}
 
-    }
-  
-    addProduct(product) {
-      // Implement functionality here
-      this.products.push(product)
-    }
-  
-    removeProduct(product) {
-      // Implement functionality here
-     const filtresProductes= this.products.filter(item=>item.name != product)
+class ShoppingCart {
+  constructor() {
+    this.products = [];
+  }
+
+  addProduct(product) {
+    // Implement functionality here
+    this.products.push(product)
+  }
+
+  removeProduct(product) {
+    // Implement functionality here
+    const filtresProductes = this.products.filter(item => item.name != product)
     this.products = filtresProductes
-    }
-  
-    searchProduct(productName) {
-      // Implement functionality here
-  
-    }
-  
-    getTotal() {
-      // Implement functionality here
-    const total= this.products.map(item=>item.price).reduce((sum,value)=>sum+value)
-    console.log(total);
-    }
-  
-    renderProducts() {
-      // Implement functionality here
-      this.products.forEach(item=>{
-        const body= document.querySelector('body')
-        const h4= document.createElement('h4')
-        const p= document.createElement('p')
-        body.appendChild(h4)
-        body.appendChild(p)
-        h4.innerText=item.name
-        p.innerText=item.price
-      })
-
-    }
-  
-    getUser() {
-      // Implement functionality here
-    }
   }
-  
-  const shoppingCart = new ShoppingCart();
-  const flatscreen = new Product("flat-screen", 5000);
-  shoppingCart.addProduct(flatscreen);
 
-                const car =new Product('Toyota',50000)
-  const smarTv = new Product('Samsung A21',8000)
-  const mobile = new Product('Iphone 11',10000)
+  searchProduct(productName) {
+    // Implement functionality here
+    const searchedProduct = this.products.filter(item => { return item.name.toLowerCase() == productName.toLowerCase() }) //
+    this.products = searchedProduct
+    this.renderProducts()
+    this.getTotal()
+  }
 
-  const islamCart = new ShoppingCart()
-  islamCart.addProduct(car)
-islamCart.addProduct(smarTv)
-islamCart.addProduct(mobile)
-islamCart.removeProduct('Toyota')
-  console.log(islamCart);
-  islamCart.getTotal()
-islamCart.renderProducts()  
-  /*
-  
-  Part 1
-Create the functionality for the ShoppingCart class.
+  getTotal() {
+    // Implement functionality here
+    const total = this.products.map(item => item.price)
+      .reduce((sum, value) => sum + value)
+    const h3 = document.createElement('h3')
+    section.appendChild(h3)
+    h3.innerText = 'Total= ' + total + ' .Kr'
+  }
 
-addProduct should add a product to the products array.
-removeProduct should remove a product from the products array.
-getTotal should get the total price of the products in the shoppingcart.
-renderProducts should render the products to html. You decide what to show and how.
-searchProduct should return an array of product that match the productName parameter
-getUser should return a promise with the data from this api:
- https://jsonplaceholder.typicode.com/users/1
-  */
+  renderProducts() {
+    // Implement functionality here
+    this.products.forEach(item => {
+      const h4 = document.createElement('h4')
+      const p = document.createElement('p')
+      main.appendChild(h4)
+      main.appendChild(p)
+      h4.innerText = item.name
+      p.innerText = item.price + ' .Kr'
+    })
+  }
+
+  getUser(userName) {
+    // Implement functionality here
+    return new Promise((resolve, reject) => {
+      fetch('https://jsonplaceholder.typicode.com/users/1')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data.name);
+          resolve(data.name)
+          userName = data.name
+
+          const user = document.createElement('h2')
+          header.appendChild(user)
+          user.innerText = 'User: ' + userName
+          this.renderProducts()
+          this.getTotal()
+        })
+    })
+
+  }
+}
+
+const shoppingCart = new ShoppingCart();
+const flatscreen = new Product("flat-screen", 5000);
+shoppingCart.addProduct(flatscreen);
+// Creat some products 
+const car = new Product('Toyota', 50000)
+const smarTv = new Product('Samsung A21', 8000)
+const mobile = new Product('Iphone 11', 10000)
+const leaneCart = new ShoppingCart() // Creat leanne shopping cart 
+// Add some products to user shopping  cart 
+leaneCart.addProduct(car)
+leaneCart.addProduct(smarTv)
+leaneCart.addProduct(mobile)
+// leaneCart.removeProduct('Toyota')
+// console.log(leaneCart);
+// leaneCart.getTotal()
+// leaneCart.renderProducts()
+leaneCart.getUser()
+
+//Search Bar Part 
+button.addEventListener('click', function () {
+  main.innerText = ''
+  section.innerText = ''
+  leaneCart.searchProduct(input.value)
+
+})
+
+
+
