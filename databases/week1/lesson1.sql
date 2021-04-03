a227ae451bd8123f8162e41d801502527cb7bb80
islam-fawzy25 <islam.fawzy25@hotmail.com>
2021-04-03 10:18:12 +0200

    homework done

---
+++ databases/week1/lesson1.sql
@@ -0,0 +1,54 @@
+-- 1.find out how many tasks are in the task table.
+select count(*)
+from task ; -- 35
+
+  -- 2. Find out how many tasks in the task table do not have a valid due date
+select count(task.id)
+from task
+  where due_date is null ; 
+
+  -- 3.Find all the tasks that are marked as done
+  select * 
+  from task 
+  join status on task.status_id = status.id 
+  where status.name  = 'Done';
+  
+  -- 4. Find all the tasks that are not marked as done
+   select * 
+  from task 
+  join status on task.status_id = status.id 
+  where status.name  != 'Done';
+  
+  -- 5. Get all the tasks, sorted with the most recently created first
+  select *
+  from task
+  order by created desc ;
+  
+  -- 6. Get the single most recently created task
+  select *
+  from task
+  order by created desc limit 1 ;
+  
+  -- 7. Get the title and due date of all tasks where the title or description contains database
+  select title , due_date 
+  from task
+  where    title   like '%database%'  or description like  '%database%'; -- how to make or  ??!!
+  
+ -- 8. Get the title and status (as text) of all tasks
+select task.title , status.name
+from task
+join status on task.status_id= status.id;
+
+-- 9 . Get the name of each status, along with a count of how many tasks have that status
+select  status.name , count(status.name) as count_tasks
+from status
+join task on status.id = task.status_id
+group by status.name;
+
+-- 10. Get the names of all statuses, sorted by the status with most tasks first
+select  status.name , count(status.name) as count_tasks
+from status
+join task on status.id = task.status_id
+group by status.name
+order by count(status.name) desc ;
+  
