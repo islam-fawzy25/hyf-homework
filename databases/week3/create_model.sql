3e4250a15b45b5bf0e046a6e14d1f007c738a06d
islam-fawzy25 <islam.fawzy25@hotmail.com>
2021-04-20 13:24:24 +0200

    delete not null from reservation table

--- databases/week3/create_model.sql
+++ databases/week3/create_model.sql
@@ -18,10 +18,10 @@ create table reservation(
 id int unsigned auto_increment primary key ,
 number_of_guests  int(1) check ( number_of_guests <= 9) not null ,  -- how to solve it ??!! how to make max number related to meal.max_reservation ??!!
 meal_id int unsigned ,
-created_date date not null,
-contact_phonenumber varchar(255) not null,
-contact_name varchar(255) not null,
-contact_email varchar(255) unique not null,
+created_date date ,
+contact_phonenumber varchar(255) ,
+contact_name varchar(255) ,
+contact_email varchar(255) unique ,
  foreign key (meal_id) references meal(id) on delete set null on update cascade
 );
 
@@ -34,4 +34,3 @@ stars int(1) not null check(stars <=5),
 created_date date not null,
 foreign key (meal_id) references meal(id) on delete set null on update cascade
 );
-
