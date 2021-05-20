39397b7d29d110b7ee483dcd93fa75ad8b69acd4
Islam Fawzy <islam.fawzy25@hotmail.com>
2021-05-20 12:37:34 +0200

    mentor feedback implemented

--- nodejs/week3/homework/warmup/index.js
+++ nodejs/week3/homework/warmup/index.js
@@ -5,11 +5,11 @@ const routerNumbers = require("./web");
 
 app.get("/", (req, res) => res.send("nodejs week3 homework"));
 
-app.use("/calculator", routerNumbers);
+
 
 //parse URL- encoded bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // router.use("/calculator", routerNumbers);
-
+app.use("/calculator", routerNumbers);
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
